41327c64886085d46850994700cac489a27e3ae2
Stephen Chung <93188963+stephen-chung-mh@users.noreply.github.com>
2022-12-22 22:23:25 +0000

    Update learn_model.py

--- thinker/thinker/learn_model.py
+++ thinker/thinker/learn_model.py
@@ -85,6 +85,8 @@ class ModelLearner():
         last_psteps = 0
         
         r_tester = None
+        all_return = None
+        
         numel_per_step = self.flags.model_batch_size * (
                 1 if not self.flags.model_batch_mode else self.flags.model_unroll_length)
 
