a0b360f9ac99aedc916f686bc6166fd78cb8ff97
Stephen Chung <stephen.chung.mh@gmail.com>
2023-01-30 18:10:00 +0000

    enabled --env Sokoban-v0

--- thinker/thinker/env.py
+++ thinker/thinker/env.py
@@ -1,8 +1,10 @@
 from collections import namedtuple
 import numpy as np
+import cv2
 import torch
 from torch.nn import functional as F
 import gym
+import gym_sokoban
 import gym_csokoban
 from thinker.gym.asyn_vector_env import AsyncVectorEnv
 from thinker import util
@@ -41,7 +43,12 @@ def Environment(flags, model_wrap=True, env_n=1, device=None):
 def PreWrap(env, name):
     if name == "cSokoban-v0":
         env = TransposeWrap(env)
-    else:
+    elif name == "Sokoban-v0":
+        env = NoopWrapper(env, cost=-0.01)
+        env = WarpFrame(env, width=80, height=80, grayscale=False)
+        env = TimeLimit_(env, max_episode_steps=120)
+        env = TransposeWrap(env)
+    else:        
         raise ValueError("Environment not supported")
 
     return env
@@ -1015,8 +1022,127 @@ class TransposeWrap(gym.ObservationWrapper):
     def observation(self, observation):
         return np.transpose(observation, axes=(2, 0, 1))
 
-# Misc.
 
+class NoopWrapper(gym.Wrapper):
+    def __init__(self, env, cost=0.):
+        gym.Wrapper.__init__(self, env)
+        env.action_space.n += 1    
+        self.cost = cost    
+        
+    def reset(self, **kwargs):
+        obs = self.env.reset(**kwargs)
+        #obs = obs[np.newaxis, :, :, :]
+        self.last_obs = obs
+        return obs
+    
+    def step(self, action):     
+        if action == 0:
+            return self.last_obs, self.cost, False, {}
+        else:            
+            obs, reward, done, info = self.env.step(action-1)
+            #obs = obs[np.newaxis, :, :, :]
+            self.last_obs = obs
+            return obs, reward, done, info 
+            
+    def get_action_meanings(self):
+        return ["NOOP",] + self.env.get_action_meanings()
+
+    def clone_state(self):
+        state = self.env.clone_state()
+        state["noop_last_obs"] = np.copy(self.last_obs)
+        return state
+
+    def restore_state(self, state):
+        self.last_obs = np.copy(state["noop_last_obs"])
+        self.env.restore_state(state)
+        return         
+
+class TimeLimit_(gym.Wrapper):
+    def __init__(self, env, max_episode_steps=None):
+        super(TimeLimit_, self).__init__(env)
+        self._max_episode_steps = max_episode_steps
+        self._elapsed_steps = None
+
+    def step(self, action):
+        assert (
+            self._elapsed_steps is not None
+        ), "Cannot call env.step() before calling reset()"
+        observation, reward, done, info = self.env.step(action)
+        self._elapsed_steps += 1
+        if self._elapsed_steps >= self._max_episode_steps:
+            info["TimeLimit.truncated"] = not done
+            done = True
+        return observation, reward, done, info
+
+    def reset(self, **kwargs):
+        self._elapsed_steps = 0
+        return self.env.reset(**kwargs)
+
+    def clone_state(self):
+        state = self.env.clone_state()
+        state["timeLimit_elapsed_steps"] = self._elapsed_steps
+        return state
+
+    def restore_state(self, state):
+        self._elapsed_steps = state["timeLimit_elapsed_steps"] 
+        self.env.restore_state(state)
+        return 
+
+
+class WarpFrame(gym.ObservationWrapper):
+    def __init__(self, env, width=84, height=84, grayscale=True, dict_space_key=None):
+        """
+        Warp frames to 84x84 as done in the Nature paper and later work.
+
+        If the environment uses dictionary observations, `dict_space_key` can be specified which indicates which
+        observation should be warped.
+        """
+        super().__init__(env)
+        self._width = width
+        self._height = height
+        self._grayscale = grayscale
+        self._key = dict_space_key
+        if self._grayscale:
+            num_colors = 1
+        else:
+            num_colors = 3
+
+        new_space = gym.spaces.Box(
+            low=0,
+            high=255,
+            shape=(self._height, self._width, num_colors),
+            dtype=np.uint8,
+        )
+        if self._key is None:
+            original_space = self.observation_space
+            self.observation_space = new_space
+        else:
+            original_space = self.observation_space.spaces[self._key]
+            self.observation_space.spaces[self._key] = new_space
+        assert original_space.dtype == np.uint8 and len(original_space.shape) == 3
+
+    def observation(self, obs):
+        if self._key is None:
+            frame = obs
+        else:
+            frame = obs[self._key]
+
+        if self._grayscale:
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        frame = cv2.resize(
+            frame, (self._width, self._height), interpolation=cv2.INTER_AREA
+        )
+        if self._grayscale:
+            frame = np.expand_dims(frame, -1)
+
+        if self._key is None:
+            obs = frame
+        else:
+            obs = obs.copy()
+            obs[self._key] = frame
+        return obs
+
+# Misc.
 
 def align(model_out, flex_t):    
     """A function that converts v1 model_out to v0 model_out"""
