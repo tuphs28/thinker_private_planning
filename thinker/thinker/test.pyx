ee5a9098fed631d8664daaa5852958f4bdaeb219
Stephen Chung <stephen.chung.mh@gmail.com>
2023-05-20 23:26:13 +0100

    remove unused files

--- thinker/thinker/test.pyx
+++
@@ -1,8 +0,0 @@
-import torch
-import gym
-
-print(torch.__file__)
-print(gym.__file__)
-print(gym.__version__)
-
-#print(gym.space.spaces.Box(low=-np.inf, high=np.inf, shape=(1, 10, 1, 1), dtype=float))
