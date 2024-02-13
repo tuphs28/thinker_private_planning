a0dd6e3d82d85e7b2d606039275bc72c421f6bcd
stephen_chung_mh <stephen.chung.mh@gmail.com>
2024-02-13 12:14:36 +0000

    .

--- thinker/thinker/wrapper.py
+++ thinker/thinker/wrapper.py
@@ -756,7 +756,7 @@ class DMSuiteEnv(gym.Env):
         info = {'truncated_done': done}
         return observation, reward, done, info
     
-    def reset(self, seed, options):
+    def reset(self, seed=None, options=None):
         timestep = self.env.reset()
         if self.rgb:
             observation = self._get_rgb_image()
