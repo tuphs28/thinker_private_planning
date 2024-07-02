fixed calculation of precision and recall in train_conv_probe.py

probing/train_conv_probe.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     out_dim = 5
     probe_args = {}
 
-    features = [f"tracked_box_next_push_onto_after"]
+    features = [f"agent_onto_after"]
     #features = ["tar_next_current_0"]
     layers = [("layer0", 32), ("layer1", 96), ("layer2", 160)]
     #layers = [("layer2", 160)]
@@ -85,11 +85,16 @@ if __name__ == "__main__":
             optimiser = torch.optim.Adam(params=convprobe.parameters(), lr=1e-4)
 
             for epoch in range(1, num_epochs+1):
+
                 positive_accs = [0 for _ in range(out_dim)]
                 prop_pos_cors = [0 for _ in range(out_dim)]
                 precisions = [0 for _ in range(out_dim)]
                 recalls = [0 for _ in range(out_dim)]
                 fones = [0 for _ in range(out_dim)]
+                conf_mat = [[0 for i in range(out_dim)] for j in range(out_dim)]
+                num_true = [0 for i in range(out_dim)]
+                num_preds = [0 for i in range(out_dim)]
+
                 for hiddens, targets, _ in train_loader:
                     hiddens = hiddens[:,-1,[layer_idx+c for c in channels],:,:]
                     targets = targets.to(torch.long)
@@ -109,36 +114,36 @@ if __name__ == "__main__":
                             full_acc += (torch.sum(logits.argmax(dim=1)==targets.view(-1,64)).item())
                             if probe_args["positive_feature"] is not None:
                                 for i in range(positive_targets.shape[0]):
-                                    if out_dim ==2:
-                                        positive_acc += 1 if positive_targets[i].sum().item()==0 else (torch.sum((logits[[i],:,:].argmax(dim=1)==targets[[i],:,:].view(-1,64))[positive_targets[[i],:,:].view(-1,64)==1]).item()) / positive_targets[i].sum().item()
-                                        prop_pos_cor +=  1 if logits[i,:,:].argmax(dim=0).sum().item()==0 else (torch.sum((logits[[i],:,:].argmax(dim=1)==targets[[i],:,:].view(-1,64))[logits[[i],:,:].argmax(dim=1)==1]).item()) / logits[i,:,:].argmax(dim=0).sum().item()
-                                    else:
-                                        for j in range(out_dim):
-                                            positive_accs[j] += 1 if (positive_targets[i]==j).sum().item()==0 else (torch.sum((logits[[i],:,:].argmax(dim=1)==targets[[i],:,:].view(-1,64))[positive_targets[[i],:,:].view(-1,64)==j]).item()) / (positive_targets[i]==j).sum().item()
-                                            prop_pos_cors[j] += 1 if (logits[i,:,:].argmax(dim=0)==j).sum().item()==0 else (torch.sum((logits[[i],:,:].argmax(dim=1)==targets[[i],:,:].view(-1,64))[logits[[i],:,:].argmax(dim=1)==j]).item()) / (logits[i,:,:].argmax(dim=0)==j).sum().item()
+                                    for j in range(out_dim):
+                                        positive_accs[j] += 1 if (positive_targets[i]==j).sum().item()==0 else (torch.sum((logits[[i],:,:].argmax(dim=1)==targets[[i],:,:].view(-1,64))[positive_targets[[i],:,:].view(-1,64)==j]).item()) / (positive_targets[i]==j).sum().item()
+                                        prop_pos_cors[j] += 1 if (logits[i,:,:].argmax(dim=0)==j).sum().item()==0 else (torch.sum((logits[[i],:,:].argmax(dim=1)==targets[[i],:,:].view(-1,64))[logits[[i],:,:].argmax(dim=1)==j]).item()) / (logits[i,:,:].argmax(dim=0)==j).sum().item()
+                                        for k in range(out_dim):
+                                            conf_mat[j][k] += torch.sum((logits[[i],:,:].argmax(dim=1)==k)[positive_targets[[i],:,:].view(-1,64)==j]).item()
+                                        num_true[j] += torch.sum(positive_targets[[i],:,:].view(-1,64)==j).item()
+                                        num_preds[j] += torch.sum((logits[[i],:,:].argmax(dim=1)==j)).item()
+                                        #print(conf_mat)
+                                        #print(num_preds)
 
                         print(f"---- Epoch {epoch} -----")
                         print("Full acc:", full_acc/(len(val_dataset.data)*64))
-                        if probe_args["positive_feature"] is not None and out_dim == 2:
+                        if probe_args["positive_feature"] is not None and out_dim == 1:
                             precision = prop_pos_cor / (len(val_dataset.data))
                             recall = positive_acc / (len(val_dataset.data))
                             fone = 0 if precision+recall==0 else (2*precision*recall) / (precision + recall)
                             print("Recall: ", recall)
                             print("Precision: ", precision)
                             print("F1: ", fone)
-                        elif probe_args["positive_feature"] is not None and out_dim != 2:
+                        elif probe_args["positive_feature"] is not None:
                             for j in range(out_dim):
                                 print(f"-- Out Dim {j} --")
-                                precisions[j] = prop_pos_cors[j] / (len(val_dataset.data))
-                                recalls[j] = positive_accs[j] / (len(val_dataset.data))
+                                precisions[j] = conf_mat[j][j] / sum(conf_mat[j])
+                                recalls[j] = conf_mat[j][j] / sum([conf_mat[k][j] for k in range(out_dim)])
                                 fones[j] = 0 if precisions[j]+recalls[j]==0 else (2*precisions[j]*recalls[j]) / (precisions[j] + recalls[j])
-                                print("Recall: ", recalls[j])
-                                print("Precision: ", precisions[j])
+                                print("Recall:", precisions[j])
+                                print("Precision:", recalls[j])
                                 print("F1: ", fones[j])
 
-            if out_dim == 2:
-                results[layer_name] = {"Acc": full_acc/(len(val_dataset.data)*64), "Precision": precision, "Recall": recall, "F1": fone, "weights": convprobe.conv.weight.view(2,len(channels)).tolist()}
-            elif out_dim == 5:
+            if out_dim != 1:
                 results_dict = {"Acc": full_acc/(len(val_dataset.data)*64)}
                 for j in range(out_dim):
                     results_dict[f"Precision_{j}"] = precisions[j]