5899cd679f5963cd2642b3bfd471b050844b8e67
tuphs28 <28tbush@gmail.com>
2024-10-09 14:42:39 +0100

    cutoff interv

--- probing/run_cutoff_interv_exps.py
+++ probing/run_cutoff_interv_exps.py
@@ -14,7 +14,104 @@ from thinker.actor_net import sample
 from thinker.util import EnvOut
 from typing import Optional
 from run_agent_interv_exps import ProbeIntervDRCNet
+box_intervs = [
+    ([], [(4,3)], [], [], [], [], [(4,3)]),
+    ([], [(3,4)], [], [], [], [], [(3,4)]),
+    ([], [(2,1)], [], [], [], [], [(2,1)]),
+    ([], [], [(4,3)], [], [], [], [(4,3)]),
+    ([], [(4,1)], [], [], [], [], [(4,1)]),
+    ([], [], [(4,3)], [], [], [], [(4,3)]), # 40
+    ([], [(4,1)], [], [], [], [], [(4,1)]),
+    ([], [(2,2)], [], [], [], [], [(2,2)]),
+    ([], [], [(2,5)], [], [], [], [(2,5)]),
+    ([], [(2,5)], [], [], [], [], [(2,5)]),
+    ([], [(2,2)], [], [], [], [], [(2,2)]), # 80
+    ([], [], [(3,4)], [], [], [], [(3,4)]),
+    ([], [(4,3)], [], [], [], [], [(4,3)]),
+    ([], [], [(4,3)], [], [], [], [(4,3)]),
+    ([], [], [(4,3)], [], [], [], [(4,3)]),
+    ([], [(2,2)], [], [], [], [], [(2,2)]), # 120
+    ([], [(2,2)], [], [], [], [], [(2,2)]),
+    ([], [(2,2)], [], [], [], [], [(2,2)]),
+    ([], [], [(2,2)], [], [], [], [(2,2)]), 
+    ([], [], [(4,3)], [], [], [], [(4,3)]),
+    ([], [], [(4,3)], [], [], [], [(4,3)]), # 160
+    ([], [(2,2)], [], [], [], [], [(2,2)]),
+    ([], [], [(2,2)], [], [], [], [(2,2)]), 
+    ([], [], [(2,5)], [], [], [], [(2,5)]), 
+    ([], [(2,5)], [], [], [], [], [(2,5)]), 
+]
 
+
+exp_paths = []
+for name, paths in zip([1], [box_intervs]):
+    olds, new_rs, new_ls, new_ds, new_us, checks, boxchecks = [], [], [], [], [], [], []
+    for old_path, new_right, new_left, new_down, new_up, checkpoint, boxpoint in paths:
+        old_path_1 = [(x,7-y) for y,x in old_path]
+        new_right_1 = [(x,7-y) for y,x in new_right]
+        new_left_1 = [(x,7-y) for y,x in new_left]
+        new_down_1 = [(x,7-y) for y,x in new_down]
+        new_up_1 = [(x,7-y) for y,x in new_up]
+        checkpoint_1 = [(x,7-y) for y,x in checkpoint]
+        boxpoint_1 = [(x,7-y) for y,x in boxpoint]
+
+        old_path_2 = [(7-y,7-x) for y,x in old_path]
+        new_right_2 = [(7-y,7-x) for y,x in new_right]
+        new_left_2 = [(7-y,7-x) for y,x in new_left]
+        new_down_2 = [(7-y,7-x) for y,x in new_down]
+        new_up_2 = [(7-y,7-x) for y,x in new_up]
+        checkpoint_2 = [(7-y,7-x) for y,x in checkpoint]
+        boxpoint_2 = [(7-y,7-x) for y,x in boxpoint]
+
+        old_path_3 = [(7-x,y) for y,x in old_path]
+        new_right_3 = [(7-x,y) for y,x in new_right]
+        new_left_3 = [(7-x,y) for y,x in new_left]
+        new_down_3 = [(7-x,y) for y,x in new_down]
+        new_up_3 = [(7-x,y) for y,x in new_up]
+        checkpoint_3 = [(7-x,y) for y,x in checkpoint]
+        boxpoint_3 = [(7-x,y) for y,x in boxpoint]
+
+        old_path_4 = [(y,7-x) for y,x in old_path]
+        new_right_4 = [(y,7-x) for y,x in new_right]
+        new_left_4 = [(y,7-x) for y,x in new_left]
+        new_down_4 = [(y,7-x) for y,x in new_down]
+        new_up_4 = [(y,7-x) for y,x in new_up]
+        checkpoint_4 = [(y,7-x) for y,x in checkpoint]
+        boxpoint_4 = [(y,7-x) for y,x in boxpoint]
+
+        old_path_5 = [(x,7-y) for y,x in old_path_4]
+        new_right_5 = [(x,7-y) for y,x in new_right_4]
+        new_left_5 = [(x,7-y) for y,x in new_left_4]
+        new_down_5 = [(x,7-y) for y,x in new_down_4]
+        new_up_5 = [(x,7-y) for y,x in new_up_4]
+        checkpoint_5 = [(x,7-y) for y,x in checkpoint_4]
+        boxpoint_5 = [(x,7-y) for y,x in boxpoint_4]
+
+        old_path_6 = [(7-y,7-x) for y,x in old_path_4]
+        new_right_6 = [(7-y,7-x) for y,x in new_right_4]
+        new_left_6 = [(7-y,7-x) for y,x in new_left_4]
+        new_down_6 = [(7-y,7-x) for y,x in new_down_4]
+        new_up_6 = [(7-y,7-x) for y,x in new_up_4]
+        checkpoint_6 = [(7-y,7-x)  for y,x in checkpoint_4]
+        boxpoint_6 = [(7-y,7-x)  for y,x in boxpoint_4]
+
+        old_path_7 = [(7-x,y) for y,x in old_path_4]
+        new_right_7 = [(7-x,y) for y,x in new_right_4]
+        new_left_7 = [(7-x,y) for y,x in new_left_4]
+        new_down_7 = [(7-x,y) for y,x in new_down_4]
+        new_up_7 = [(7-x,y) for y,x in new_up_4]
+        checkpoint_7 = [(7-x,y) for y,x in checkpoint_4]
+        boxpoint_7 = [(7-x,y) for y,x in boxpoint_4]
+
+        olds += [old_path, old_path_1, old_path_2, old_path_3, old_path_4, old_path_5, old_path_6, old_path_7]
+        new_rs += [new_right, new_right_1, new_right_2, new_right_3, new_right_4, new_right_5, new_right_6, new_right_7]
+        new_ls += [new_left, new_left_1, new_left_2, new_left_3, new_left_4, new_left_5, new_left_6, new_left_7]
+        new_ds += [new_down, new_down_1, new_down_2, new_down_3, new_down_4, new_down_5, new_down_6, new_down_7]
+        new_us += [new_up, new_up_1, new_up_2, new_up_3, new_up_4, new_up_5, new_up_6, new_up_7]
+        checks += [checkpoint, checkpoint_1, checkpoint_2, checkpoint_3, checkpoint_4, checkpoint_5, checkpoint_6, checkpoint_7]
+        boxchecks += [boxpoint, boxpoint_1, boxpoint_2, boxpoint_3, boxpoint_4, boxpoint_5, boxpoint_6, boxpoint_7]
+    exp_paths.append([name, (olds, new_rs, new_ls, new_ds, new_us, checks, boxchecks)])
+    
 num_trials = 200
 fails = []
 flags = util.create_setting(args=[], save_flags=False, wrapper_type=1) 
